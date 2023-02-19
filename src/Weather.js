784eaa1d8bc45b701b4f77e5c8352ece47e41555
habybheart1 <akintobiabibat@yahoo.com>
2023-02-19 15:38:22 +0100

    update weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,17 @@ import React from "react";
 export default function Weather() {
   return (
     <div className="Weather">
-      <div className="container">my name is</div>
+      <div className="container">
+        <form>
+          <input type="search" placeholder="Enter a city" />
+          <input type="submit" value="search" />
+        </form>
+        <h1>Nigeria</h1>
+        <ul>
+          <li>Sun, 19th Feb., 2023</li>
+          <li>Cloudy</li>
+        </ul>
+      </div>
     </div>
   );
 }
